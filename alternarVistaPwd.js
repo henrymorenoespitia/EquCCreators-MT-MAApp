569dcc2766a41b962377d1dd3540a7b8d67b45f4
ruth1605 <rrobayo@uninorte.edu.co>
2020-12-03 21:41:47 -0500

    Con recuperar contraseña

--- alternarVistaPwd.js
+++ alternarVistaPwd.js
@@ -1,30 +1,29 @@
 function alternarVista() {
-    var contrasenaNueva = document.getElementById("thePassword");
-    if (contrasenaNueva.type === "password") {
-      contrasenaNueva.type = "text";
-    } else {
-      contrasenaNueva.type = "password";
-    }
-
-    var repetirContrasena = document.getElementById("repPassword");
-    if (repetirContrasena.type === "password") {
-      repetirContrasena.type = "text";
-    } else {
-      repetirContrasena.type = "password";
-    }
-
+  var contrasenaNueva = document.getElementById('thePassword');
+  if (contrasenaNueva.type === 'password') {
+    contrasenaNueva.type = 'text';
+  } else {
+    contrasenaNueva.type = 'password';
   }
 
-  
-  function mouseAbajo(ver){
-    var contrasenaNueva_1 = document.getElementById("thePassword");
-    if(ver){
-      contrasenaNueva_1.type = "text";
-    }else{contrasenaNueva_1.type = "password";}
-    
+  var repetirContrasena = document.getElementById('repPassword');
+  if (repetirContrasena.type === 'password') {
+    repetirContrasena.type = 'text';
+  } else {
+    repetirContrasena.type = 'password';
   }
+}
 
-function mouseArriba(){
-  var contrasenaNueva_2 = document.getElementById("thePassword");
-  contrasenaNueva_2.type = "password";
+function mouseAbajo(ver) {
+  var contrasenaNueva_1 = document.getElementById('thePassword');
+  if (ver) {
+    contrasenaNueva_1.type = 'text';
+  } else {
+    contrasenaNueva_1.type = 'password';
+  }
+}
+
+function mouseArriba() {
+  var contrasenaNueva_2 = document.getElementById('thePassword');
+  contrasenaNueva_2.type = 'password';
 }
